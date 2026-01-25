f5c58ce1a4173951d7eed5cc07e69a58de60e7f0
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2026-01-25 20:43:07 +0100

    Add matchers for pending and rejected in authSlice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -11,25 +11,28 @@ const authSlice = createSlice({
     token: null,
     isLoggedIn: false,
     isRefreshing: false,
+    isError: false,
+    isLoading: false,
   },
   extraReducers: (builder) => {
     builder
       .addCase(register.fulfilled, (state, active) => {
-        console.log(active);
-
         state.user = active.payload.user;
         state.isLoggedIn = true;
         state.token = active.payload.token;
+        state.isLoading = false;
       })
       .addCase(login.fulfilled, (state, active) => {
         state.user = active.payload.user;
         state.isLoggedIn = true;
         state.token = active.payload.token;
+        state.isLoading = false;
       })
       .addCase(logout.fulfilled, (state) => {
         state.user = { name: null, email: null };
         state.isLoggedIn = false;
         state.token = "";
+        state.isLoading = false;
       })
       .addCase(refresh.pending, (state) => {
         state.isRefreshing = true;
@@ -41,7 +44,24 @@ const authSlice = createSlice({
       })
       .addCase(refresh.rejected, (state) => {
         state.isRefreshing = false;
-      });
+        state.user = { name: null, email: null };
+        state.isLoggedIn = false;
+        state.token = "";
+      })
+      .addMatcher(
+        ({ type }) => type.startsWith("auth/") && type.endsWith("/pending"),
+        (state) => {
+          state.isLoading = true;
+          state.isError = false;
+        },
+      )
+      .addMatcher(
+        ({ type }) => type.startsWith("auth/") && type.endsWith("/rejected"),
+        (state) => {
+          state.isLoading = false;
+          state.isError = true;
+        },
+      );
   },
 });
 
